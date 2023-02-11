e965ba336d312fca8dcea9afff4f108d45396cda
JULIANO ROHDE <julianorohde@hotmail.com>
2023-02-11 15:10:07 -0400

    Installing stimulus

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,6 +1,4 @@
-import { application } from "./application";
-
-//import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
